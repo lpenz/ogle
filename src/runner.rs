5b25a8991fadbd60070877c3ba5512255d2d72d8
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-09-24 22:34:54 +0100

    Evaluate process output while the process is running
    - Hold last result in vec
    - Compare lines as process is running
    - If difference if found, print all stored lines and switch to
      printing everything

--- src/runner.rs
+++ src/runner.rs
@@ -2,8 +2,8 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
-use std::io::stdout;
-use std::io::Write;
+use std::io;
+use std::io::BufRead;
 use std::process::Command;
 use std::process::Stdio;
 
@@ -12,23 +12,39 @@ use crate::cli::Cli;
 pub fn buildcmd(cli: &Cli) -> Command {
     let mut cmd = Command::new(&cli.command[0]);
     cmd.stdin(Stdio::null());
+    cmd.stdout(Stdio::piped());
+    // cmd.stderr(Stdio::piped());
     cmd.args(cli.command.iter().skip(1));
     cmd
 }
 
 pub fn run(cli: &Cli) {
-    let mut lastout = None;
+    let mut lastout = vec![];
     loop {
-        let mut cmd = Command::new(&cli.command[0]);
-        let out = cmd.output().ok();
-        if out != lastout {
-            lastout = out;
-            if let Some(o) = &lastout {
+        let mut cmd = buildcmd(&cli);
+        let mut child = cmd.spawn().expect("error running command");
+        let stdout = child.stdout.take().expect("error taking stdout");
+        // let stderr = child.stderr().take().expect("error taking stdout");
+        let bufstdout = io::BufReader::new(stdout);
+
+        let mut currout = vec![];
+        let mut different = false;
+        for (iline, lineres) in bufstdout.lines().enumerate() {
+            let line = lineres.expect("error reading line");
+            currout.push(line);
+            if different {
+                println!("{}", currout[iline]);
+                continue;
+            }
+            if lastout.len() < iline + 1 || currout[iline] != lastout[iline] {
+                // Print everything so far
                 println!("{}", chrono::offset::Local::now());
-                stdout()
-                    .write_all(&o.stdout)
-                    .expect("error writing to stdout");
+                for l in &currout {
+                    println!("{}", l);
+                }
+                different = true;
             }
         }
+        lastout = currout;
     }
 }
