36a90e7d8c689a16c8652979286b2c5e4eeb92d2
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-10-03 17:07:13 +0100

    Custom Cli parser functions for better errors
    Not specifying a command is an error, for instance.

--- src/bin/ogle.rs
+++ src/bin/ogle.rs
@@ -2,8 +2,6 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
-use structopt::StructOpt;
-
 fn main() {
     let args = ogle::cli::Cli::from_args();
     ogle::runner::run(&args);
--- src/cli.rs
+++ src/cli.rs
@@ -2,21 +2,61 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
+use std::env;
+use std::ffi::OsString;
+use std::process;
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 pub struct Cli {
     /// Period to sleep between executions
-    #[structopt(short = "p", long = "period", default_value = "1")]
+    #[structopt(short, long, default_value = "1")]
     pub period: u64,
 
     /// The command to run
     pub command: Vec<String>,
 }
 
+impl Cli {
+    pub fn from_args() -> Cli {
+        Cli::from_iter_safe(&mut env::args_os()).unwrap_or_else(|e| {
+            eprintln!("{}", e.message);
+            process::exit(1);
+        })
+    }
+
+    pub fn from_iter_safe<I>(iter: I) -> Result<Cli, clap::Error>
+    where
+        Self: Sized,
+        I: IntoIterator,
+        I::Item: Into<OsString> + Clone,
+    {
+        let clap = Cli::clap().get_matches_from_safe(iter)?;
+        let cli = Cli::from_clap(&clap);
+        if cli.command.is_empty() {
+            Err(clap::Error::with_description(
+                "No command specified
+
+For more information try --help",
+                clap::ErrorKind::EmptyValue,
+            ))
+        } else {
+            Ok(cli)
+        }
+    }
+}
+
+#[test]
+fn empty() {
+    let cli = Cli::from_iter_safe(vec!["ogle"]);
+    assert!(cli.is_err());
+}
+
 #[test]
 fn dashes() {
-    let cli = Cli::from_iter(vec!["ogle", "--", "ls", "-l"]);
+    let cli = Cli::from_iter_safe(vec!["ogle", "--"]);
+    assert!(cli.is_err());
+    let cli = Cli::from_iter_safe(vec!["ogle", "--", "ls", "-l"]).unwrap();
     assert_eq!(cli.command[0], "ls");
     assert_eq!(cli.command[1], "-l");
     assert_eq!(cli.command.len(), 2);
@@ -25,8 +65,8 @@ fn dashes() {
 
 #[test]
 fn period() {
-    let cli = Cli::from_iter(vec!["ogle", "-p", "5", "--", "ls", "-l"]);
+    let cli = Cli::from_iter_safe(vec!["ogle", "-p", "5", "--", "ls", "-l"]).unwrap();
     assert_eq!(cli.period, 5);
-    let cli = Cli::from_iter(vec!["ogle", "--period", "7", "--", "ls", "-l"]);
+    let cli = Cli::from_iter_safe(vec!["ogle", "--period", "7", "--", "ls", "-l"]).unwrap();
     assert_eq!(cli.period, 7);
 }
