833bf50768dcd9f07ac0d63eb532774b61217d45
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-01-21 12:37:35 +0000

    Cargo.*: update dependencies; bump version to 1.4.4
    Also use nixos-unstable in flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "Execute a command periodically, showing the output only when it changes";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default =
       with import nixpkgs { system = "x86_64-linux"; };
 
       rustPlatform.buildRustPackage {
         pname = "ogle";
-        version = "1.4.3";
+        version = "1.4.4";
         src = self;
         cargoLock.lockFile = ./Cargo.lock;
       };
