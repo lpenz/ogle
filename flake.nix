d196f9d314089cf90ecd0f279271ef7036000afc
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-02-13 23:01:04 +0000

    flake.nix: multi-platform support with flake-utils

--- flake.nix
+++ flake.nix
@@ -1,15 +1,23 @@
 {
   description = "Execute a command periodically, showing the output only when it changes";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default =
-      with import nixpkgs { system = "x86_64-linux"; };
 
-      rustPlatform.buildRustPackage {
-        pname = "ogle";
-        version = "1.4.4";
-        src = self;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      rec {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+            pname = "ogle";
+            version = "1.4.4";
+            src = self;
+            cargoLock.lockFile = ./Cargo.lock;
+        };
+      }
+    );
 }
