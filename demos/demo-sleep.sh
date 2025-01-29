8733ba194d8ef19d58843b5d12470517d03353bf
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-01-29 22:16:49 +0000

    Fix demo scripts and update gif with latest version
    Also shfmt scripts.

--- demos/demo-sleep.sh
+++ demos/demo-sleep.sh
@@ -5,22 +5,21 @@ function typeogle {
     cmd="ogle $1"
     printf "$ "
     sleep 0.2
-    for (( i=0; i<"${#cmd}"; i++ )); do
+    for ((i = 0; i < "${#cmd}"; i++)); do
         sleep 0.1
         printf "%c" "${cmd:$i:1}"
     done
     echo
 }
 
-typeogle '-p 3 -c "date; sleep 3"'
+typeogle '-p 3 -- /bin/bash -c "date; sleep 3"'
 (
-"$CARGO_TARGET_DIR/debug/ogle" -p 3 -c 'date; sleep 3' &
-pidogle="$!"
-trap 'kill $pidogle; wait $pidogle' EXIT
+    "$CARGO_TARGET_DIR/debug/ogle" -p 3 -- /bin/bash -c 'date; sleep 3' &
+    pidogle="$!"
+    trap 'kill $pidogle; wait $pidogle' EXIT
 
-sleep 16.5
+    sleep 16.5
 )
 
 printf '^C\n$ '
 sleep 1
-
--- demos/run-all.sh
+++ demos/run-all.sh
@@ -6,6 +6,6 @@ for demoscript in ./demos/demo-*.sh; do
     rm -f demo.cast
     asciinema rec -c "$demoscript" demo.cast
     base="${demoscript%.*}"
-    docker run --rm -v "$PWD:/data" asciinema/asciicast2gif -S1 demo.cast "${base}.gif"
+    docker run --rm -v "$PWD:/data" docker.io/asciinema2/asciicast2gif -S1 demo.cast "${base}.gif"
 done
 rm -f demo.cast
