542cc8b3e1dcdb82656bf95c66040de6932aed36
Leandro Lisboa Penz <lpenz@lpenz.org>
2026-02-02 14:05:25 +0000

    Migrate to crossterm with a tokio thread that pools the keyboard
    We were using line streaming on stdin, but that doesn't provide a
    cancellation point, which is why the -z flag was not working.
    
    Fixes #9

--- src/user_wrapper.rs
+++ src/user_wrapper.rs
@@ -7,25 +7,27 @@
 //! For now we just check if the user has typed ENTER, which makes
 //! ogle exit after the current run is over.
 
-use is_terminal::IsTerminal;
+use crossterm::event::{self, Event};
+use crossterm::tty::IsTty;
 use pin_project::pin_project;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use tokio::io::Stdin;
-use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::io;
+use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use tokio::time::{Duration, sleep};
 use tokio_stream::Stream;
-use tokio_stream::wrappers::LinesStream;
 use tracing::instrument;
 
-/// A wrapper for [`tokio_stream::wrappers::LinesStream`] with an
-/// inner [`tokio::io::Stdin`].
+/// A wrapper for `crossterm` that polls the keyboard and provides the
+/// keypress in a tokio stream.
 ///
 /// Also provides a virtual implementation for use in tests.
 #[pin_project(project = UserStreamProj)]
 #[derive(Debug, Default)]
 pub enum UserStream {
     /// A real implementation that reads a line from stdin.
-    Real(LinesStream<BufReader<Stdin>>),
+    Real(mpsc::UnboundedReceiver<String>, JoinHandle<()>),
     /// A virtual implementation that doesn't do anything.
     #[default]
     Virtual,
@@ -34,10 +36,22 @@ pub enum UserStream {
 impl UserStream {
     pub fn new_real() -> Option<UserStream> {
         let stdin = io::stdin();
-        if stdin.is_terminal() {
-            let bufstdin = BufReader::new(stdin);
-            let linesstream = LinesStream::new(bufstdin.lines());
-            Some(UserStream::Real(linesstream))
+        if stdin.is_tty() {
+            let (tx, rx) = mpsc::unbounded_channel::<String>();
+            let handle = tokio::spawn(async move {
+                loop {
+                    if matches!(event::poll(Duration::from_secs(0)), Ok(true))
+                        && let Ok(Event::Key(key_event)) = event::read()
+                    {
+                        tx.send(format!("{}", key_event.code))
+                            .unwrap_or_else(|e| panic!("error reading key event: {}", e));
+                    } else {
+                        // We tokio-sleep here to provide a cancellation point:
+                        sleep(Duration::from_millis(127)).await;
+                    }
+                }
+            });
+            Some(UserStream::Real(rx, handle))
         } else {
             None
         }
@@ -55,14 +69,12 @@ impl Stream for UserStream {
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
         match this {
-            UserStreamProj::Real(linesstream) => {
-                let next = Pin::new(linesstream).poll_next(cx);
+            UserStreamProj::Real(rx, _) => {
+                let next = Pin::new(rx).poll_recv(cx);
                 match next {
-                    Poll::Ready(Some(Ok(s))) => Poll::Ready(Some(s)),
+                    Poll::Ready(Some(s)) => Poll::Ready(Some(s)),
                     Poll::Ready(None) => Poll::Ready(None),
                     Poll::Pending => Poll::Pending,
-                    // End stream in case of error:
-                    Poll::Ready(_) => Poll::Ready(None),
                 }
             }
             UserStreamProj::Virtual {} => Poll::Ready(None),
